7ddb693c09d30b4a2108c8e6715913a910eb624c
Omar Martins <omarmartinsabreu@gmail.com>
2024-11-16 21:00:30 -0300

    corrige ediçao de pagina pq ser voluntario

--- src/js/informative-pages.js
+++ src/js/informative-pages.js
@@ -1,12 +1,11 @@
-import { PageContent } from "../../js/models/pageContent.js";
+import { PageContent } from "./models/pageContent.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
-    
+
     const title = document.querySelector('#title').textContent.trim();
     const pageContent = new PageContent();
     const content = await pageContent.findByTitle(title).then(content => content[0]);
 
     document.querySelector('#page-content').innerHTML = content.content;
-
 
 });
