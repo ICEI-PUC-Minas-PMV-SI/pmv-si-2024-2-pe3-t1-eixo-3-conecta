e8fb784740ad34ae4a782c9e69bb927077640cad
Duque <devgduque@gmail.com>
2024-11-21 07:24:20 -0300

    Retira coisas irrelevantes do perfil do usuário (Seção de Midia Social)

--- src/pages/pagina-do-voluntario/pagina-do-voluntario.js
+++ src/pages/pagina-do-voluntario/pagina-do-voluntario.js
@@ -1,142 +1,140 @@
-import {Task} from "../../js/models/task.js";
-import {Review} from "../../js/models/review.js";
-import {Candidate} from "../../js/models/candidate.js";
-import {VerticalTaskCard} from "../../components/vertical-task-card/vertical-task-card.js";
-import {HorizontalTaskCard} from "../../components/horizontal-task-card/horizontal-task-card.js";
-import {getSession} from "../../js/models/session.js";
+import { Task } from "../../js/models/task.js";
+import { Review } from "../../js/models/review.js";
+import { Candidate } from "../../js/models/candidate.js";
+import { VerticalTaskCard } from "../../components/vertical-task-card/vertical-task-card.js";
+import { HorizontalTaskCard } from "../../components/horizontal-task-card/horizontal-task-card.js";
+import { getSession } from "../../js/models/session.js";
 
-const descriptions = document.querySelectorAll('.task-description > p');
+const descriptions = document.querySelectorAll(".task-description > p");
 
-descriptions.forEach(description => {
-    const showMoreButton = description.nextElementSibling; // Assuming the button is a sibling element
+descriptions.forEach((description) => {
+  const showMoreButton = description.nextElementSibling; // Assuming the button is a sibling element
 
-    // check if the description needs a 'Show More' button
-    if(description.offsetHeight < description.scrollHeight) {
-        showMoreButton.style.display = 'block';
-    }
+  // check if the description needs a 'Show More' button
+  if (description.offsetHeight < description.scrollHeight) {
+    showMoreButton.style.display = "block";
+  }
 });
 
 const getCandidateId = async () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const ongId =  urlParams.get('id');
-    if (ongId) return ongId;
+  const urlParams = new URLSearchParams(window.location.search);
+  const ongId = urlParams.get("id");
+  if (ongId) return ongId;
 
-    const token = window.localStorage.getItem("token");
-    const session = await getSession(token);
-    return await session[0].userId;
-}
+  const token = window.localStorage.getItem("token");
+  const session = await getSession(token);
+  return await session[0].userId;
+};
 
 const getCandidateData = async () => {
-    const id = await getCandidateId();
-    const candidate = new Candidate();
-    return await candidate.findById(id);
-}
+  const id = await getCandidateId();
+  const candidate = new Candidate();
+  return await candidate.findById(id);
+};
 
 function addFeedbackItem(name, date, text) {
-    const feedbackItem = document.createElement('div');
-    feedbackItem.className = 'feedback-item';
+  const feedbackItem = document.createElement("div");
+  feedbackItem.className = "feedback-item";
 
-    const feedbackHeader = document.createElement('div');
-    feedbackHeader.className = 'feedback-header';
+  const feedbackHeader = document.createElement("div");
+  feedbackHeader.className = "feedback-header";
 
-    const feedbackName = document.createElement('p');
-    feedbackName.className = 'feedback-name';
-    feedbackName.innerText = name;
+  const feedbackName = document.createElement("p");
+  feedbackName.className = "feedback-name";
+  feedbackName.innerText = name;
 
-    const feedbackDate = document.createElement('p');
-    feedbackDate.className = 'feedback-date';
-    const dateObj = new Date(date);
-    const dateToLocale = dateObj.toLocaleDateString('pt-BR', { month: 'long', year: 'numeric' });
-    const formattedDate = dateToLocale.replace(' de ', ', ');
-    feedbackDate.innerText = formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+  const feedbackDate = document.createElement("p");
+  feedbackDate.className = "feedback-date";
+  const dateObj = new Date(date);
+  const dateToLocale = dateObj.toLocaleDateString("pt-BR", {
+    month: "long",
+    year: "numeric",
+  });
+  const formattedDate = dateToLocale.replace(" de ", ", ");
+  feedbackDate.innerText =
+    formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
 
-    feedbackHeader.appendChild(feedbackName);
-    feedbackHeader.appendChild(feedbackDate);
+  feedbackHeader.appendChild(feedbackName);
+  feedbackHeader.appendChild(feedbackDate);
 
-    const feedbackText = document.createElement('p');
-    feedbackText.className = 'feedback-text';
-    feedbackText.innerText = text;
+  const feedbackText = document.createElement("p");
+  feedbackText.className = "feedback-text";
+  feedbackText.innerText = text;
 
-    feedbackItem.appendChild(feedbackHeader);
-    feedbackItem.appendChild(feedbackText);
+  feedbackItem.appendChild(feedbackHeader);
+  feedbackItem.appendChild(feedbackText);
 
-    const feedbackWrapper = document.querySelector('.feedback-wrapper');
-    feedbackWrapper.appendChild(feedbackItem);
+  const feedbackWrapper = document.querySelector(".feedback-wrapper");
+  feedbackWrapper.appendChild(feedbackItem);
 }
 
 const populateCandidateData = async () => {
-    const candidateData = await getCandidateData();
-    const name = document.getElementById('candidate-name');
-    const facebookLink = document.getElementById('facebook-link');
-    const instagramLink = document.getElementById('instagram-link');
-    const twitterLink = document.getElementById('twitter-link');
-    const about = document.getElementById('about-text');
-    const tasksWrapper = document.getElementById('task-wrapper');
+  const candidateData = await getCandidateData();
+  const name = document.getElementById("candidate-name");
+  const about = document.getElementById("about-text");
+  const tasksWrapper = document.getElementById("task-wrapper");
 
-    if(!candidateData.facebook) facebookLink.style.display = 'none';
-    if(!candidateData.instagram) instagramLink.style.display = 'none';
-    if(!candidateData.twitter) twitterLink.style.display = 'none';
+  name.innerHTML = candidateData.name;
+  about.innerHTML = candidateData.about;
 
-    name.innerHTML = candidateData.name;
-    facebookLink.href = candidateData.facebook;
-    instagramLink.href = candidateData.instagram;
-    twitterLink.href = candidateData.twitter;
-    about.innerHTML = candidateData.about;
+  const candidate = new Candidate();
 
-    const candidate = new Candidate();
+  const review = new Review();
+  const reviews = await review.findAllAnsweredByCandidateId(candidateData.id);
 
-    const review = new Review();
-    const reviews = await review.findAllAnsweredByCandidateId(candidateData.id);
+  for await (const review of reviews) {
+    const candidateData = await candidate.findById(review.candidateId);
+    addFeedbackItem(candidateData.name, review.createdAt, review.comment);
+  }
 
-    for await (const review of reviews) {
-        const candidateData = await candidate.findById(review.candidateId);
-        addFeedbackItem(candidateData.name, review.createdAt, review.comment);
+  const task = new Task();
+  const tasks = await task.findByCandidateId(candidateData.id);
+
+  tasks.forEach((task) => {
+    const verticalTaskCard = new VerticalTaskCard();
+
+    verticalTaskCard.name = task.name;
+    verticalTaskCard.description = task.description;
+    if (task.type.toLowerCase() === "presencial") {
+      verticalTaskCard.type = candidateData.city + ", " + candidateData.state;
+    } else {
+      verticalTaskCard.type = task.type;
+      let upperCaseType = verticalTaskCard.type;
+      verticalTaskCard.type =
+        upperCaseType.charAt(0).toUpperCase() + upperCaseType.slice(1);
     }
+    verticalTaskCard.destination = `../candidatar-a-demanda/candidatar-a-demanda.html?id=${task.id}`;
+    verticalTaskCard.owner = candidateData.name;
+    verticalTaskCard.image = candidateData.image;
+    verticalTaskCard.addres =
+      candidateData.street + ", " + candidateData.number;
 
-    const task = new Task();
-    const tasks = await task.findByCandidateId(candidateData.id);
+    tasksWrapper.appendChild(verticalTaskCard);
+  });
 
-    tasks.forEach(task => {
-        const verticalTaskCard = new VerticalTaskCard();
+  tasks.forEach((task) => {
+    const horizontalTaskCard = new HorizontalTaskCard();
 
-        verticalTaskCard.name = task.name;
-        verticalTaskCard.description = task.description;
-        if(task.type.toLowerCase() === 'presencial') {
-                verticalTaskCard.type = candidateData.city+', '+candidateData.state;
-        } else {
-                verticalTaskCard.type = task.type;
-                let upperCaseType = verticalTaskCard.type;
-                verticalTaskCard.type = upperCaseType.charAt(0).toUpperCase() + upperCaseType.slice(1)
-        }
-        verticalTaskCard.destination = `../candidatar-a-demanda/candidatar-a-demanda.html?id=${task.id}`;
-        verticalTaskCard.owner = candidateData.name
-        verticalTaskCard.image = candidateData.image;
-        verticalTaskCard.addres = candidateData.street+', '+candidateData.number
+    horizontalTaskCard.name = task.name;
+    horizontalTaskCard.description = task.description;
+    if (task.type.toLowerCase() === "presencial") {
+      horizontalTaskCard.type = candidateData.city + ", " + candidateData.state;
+    } else {
+      horizontalTaskCard.type = task.type;
+      let upperCaseType = horizontalTaskCard.type;
+      horizontalTaskCard.type =
+        upperCaseType.charAt(0).toUpperCase() + upperCaseType.slice(1);
+    }
+    horizontalTaskCard.destination = `../candidatar-a-demanda/candidatar-a-demanda.html?id=${task.id}`;
+    horizontalTaskCard.owner = candidateData.name;
+    horizontalTaskCard.image = candidateData.image;
+    horizontalTaskCard.addres =
+      candidateData.street + ", " + candidateData.number;
 
-        tasksWrapper.appendChild(verticalTaskCard);
-    });
+    tasksWrapper.appendChild(horizontalTaskCard);
+  });
+};
 
-    tasks.forEach(task => {
-        const horizontalTaskCard = new HorizontalTaskCard();
-
-        horizontalTaskCard.name = task.name;
-        horizontalTaskCard.description = task.description;
-        if(task.type.toLowerCase() === 'presencial') {
-                horizontalTaskCard.type = candidateData.city+', '+candidateData.state;
-        } else {
-            horizontalTaskCard.type = task.type;
-            let upperCaseType = horizontalTaskCard.type;
-            horizontalTaskCard.type = upperCaseType.charAt(0).toUpperCase() + upperCaseType.slice(1)
-        }
-        horizontalTaskCard.destination = `../candidatar-a-demanda/candidatar-a-demanda.html?id=${task.id}`;
-        horizontalTaskCard.owner = candidateData.name
-        horizontalTaskCard.image = candidateData.image;
-        horizontalTaskCard.addres = candidateData.street+', '+candidateData.number
-
-
-        tasksWrapper.appendChild(horizontalTaskCard);
-
-    });
-}
-
-populateCandidateData().then().catch(err => console.log(err));
+populateCandidateData()
+  .then()
+  .catch((err) => console.log(err));
